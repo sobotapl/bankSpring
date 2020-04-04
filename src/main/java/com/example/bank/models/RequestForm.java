1081113c5d81fcac854ed9b4cb1142eba138a171
piotr <piosoboci@gmail.com>
2020-04-04 13:54:00 +0200

    - lombok added

--- src/main/java/com/example/bank/models/RequestForm.java
+++ src/main/java/com/example/bank/models/RequestForm.java
@@ -1,49 +1,11 @@
 package com.example.bank.models;
+import lombok.Data;
 
+@Data
 public class RequestForm {
     private int monthCash;
     private int minusCash;
     private int creditTime;
     private int creditCash;
 
-    public RequestForm(int monthCash, int minusCash, int creditTime, int creditCash) {
-        this.monthCash = monthCash;
-        this.minusCash = minusCash;
-        this.creditTime = creditTime;
-        this.creditCash = creditCash;
-    }
-
-    public RequestForm() {}
-
-    public int getMonthCash() {
-        return monthCash;
-    }
-
-    public void setMonthCash(int monthCash) {
-        this.monthCash = monthCash;
-    }
-
-    public int getMinusCash() {
-        return minusCash;
-    }
-
-    public void setMinusCash(int minusCash) {
-        this.minusCash = minusCash;
-    }
-
-    public int getCreditTime() {
-        return creditTime;
-    }
-
-    public void setCreditTime(int creditTime) {
-        this.creditTime = creditTime;
-    }
-
-    public int getCreditCash() {
-        return creditCash;
-    }
-
-    public void setCreditCash(int creditCash) {
-        this.creditCash = creditCash;
-    }
 }
